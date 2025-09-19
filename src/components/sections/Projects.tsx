37bfcc30bfbe22d85c2f6f89a65cf17ccf4731ed
kiranmayee-abbireddy <kiranmayee118@gmail.com>
2025-09-19 21:55:51 +0530

    added new design for projects

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -211,7 +211,7 @@ const Projects: React.FC = () => {
                   title={project.title}
                   className="w-full h-full border-0"
                   loading="lazy"
-                  sandbox="allow-scripts allow-same-origin allow-forms"
+                  sandbox="allow-scripts allow-same-origin"
                   tabIndex={-1} // prevent focus
                 />
                 <a
